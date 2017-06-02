14091c675e046016899f341584b075fe2ce8fdaf
b1anker <narutowontstop@gmail.com>
2017-06-02 23:11:23 +0800

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
-import { Router, Route, hashHistory } from 'react-router';
-import { connect } from 'react-redux';
+import {Router, Route, hashHistory} from 'react-router';
+import {connect} from 'react-redux';
 import MediaQuery from 'react-responsive';
 import PcIndex from './components/pc/index';
 import MobileIndex from './components/mobile/index';
 import * as actions from './store/actions';
 
 import '@/scss/tool.scss';
-
 
 class App extends React.Component {
 	update() {
@@ -15,9 +14,9 @@ class App extends React.Component {
 	}
 
 	render() {
-		const { updateSign } =  this.props;
+		const {updateSign} = this.props;
 		return (
-			<div onClick={ this.update.bind(this) }>
+			<div onClick={this.update.bind(this)}>
 				<MediaQuery query='(min-device-width: 1224px)'>
 					<PcIndex/>
 				</MediaQuery>
@@ -30,20 +29,18 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  hasLogin: React.PropTypes.bool.isRequired,
-  updateSign: React.PropTypes.func.isRequired
+	hasLogin: React.PropTypes.bool.isRequired,
+	updateSign: React.PropTypes.func.isRequired
 }
 
 function mapStateToProps(state) {
-  return {
-    hasLogin: state.updateSign.hasLogin
-  }
+	return {hasLogin: state.updateSign.hasLogin}
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    updateSign: () => dispatch(actions.Has_Logined)
-  }
+	return {
+		updateSign: () => dispatch(actions.Has_Logined)
+	}
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
